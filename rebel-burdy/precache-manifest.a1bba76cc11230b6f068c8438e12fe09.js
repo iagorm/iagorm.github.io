aba251335f7a36a2132747e506c2397b2b590f2b
Iagor Moraes <iagor@rebellionpay.com>
2019-05-03 11:55:09 -0300

    added rebel-burdy

---
+++ rebel-burdy/precache-manifest.a1bba76cc11230b6f068c8438e12fe09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d830291b3c6f2379cdb226368a88f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0856130a15afc7717d4e",
+    "url": "/static/css/main.a10b48ed.chunk.css"
+  },
+  {
+    "revision": "6dc1ca0a67a3fce258c8",
+    "url": "/static/js/2.33856d8e.chunk.js"
+  },
+  {
+    "revision": "0856130a15afc7717d4e",
+    "url": "/static/js/main.29c0884a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
